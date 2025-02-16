5829a27b343003a4eecb673ca6b7483d5c307ef2
kaleeswaran <kalesmca@gmail.com>
2025-02-16 12:51:35 +0530

    Query program is done

--- department/src/main/java/com/College/Practice/QuerySum.java
+++ department/src/main/java/com/College/Practice/QuerySum.java
@@ -2,7 +2,29 @@ package com.College.Practice;
 
 public class QuerySum {
     public static void main(String[] args){
-        System.out.println("Welcome");
+        int[][] query = {{1,3,2},{2,5,3},{5,6,-1}};
+        int n =7;
+        int[] input = new int[n];
+        for(int i=0;i<query.length;i++){
+            int s = query[i][0];
+            int e = query[i][1];
+            int v = query[i][2];
+            input[s] +=v;
+            if(e<n-1){
+                input[e+1] -= v;
+            }
+        }
+        int[] prefix = new int[n];
+        prefix[0] = input[0];
+        for (int i=1;i<n;i++){
+            prefix[i] = prefix[i-1]+input[i];
+
+        }
+        System.out.println("Reulst = ");
+        for(int i=0;i<n;i++){
+            System.out.print(" "+prefix[i]);
+        }
+
 
     }
 }
